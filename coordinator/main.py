9862a6f0a0ba69f34d181fe54264c69d20e7e1bc
dhamma-dev <dhamma-dev@users.noreply.github.com>
2025-09-26 10:09:26 -0700

    Update main.py
    updated port to 8303

--- coordinator/main.py
+++ coordinator/main.py
@@ -135,4 +135,4 @@ def list_results():
 if __name__ == '__main__':
     # This is for local development only.
     # In production, use a proper WSGI server like Gunicorn.
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8308, debug=True)
